e3a58ce37c57d0a1a70afce1411d67697f615000
Annie <quratulain1341999>
2025-03-20 21:11:58 -0700

    Quratulain Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'SplashScreen.dart';
+//import 'SplashScreen.dart';
+import 'onboarding0.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: SplashScreen(),
+    home: Onboarding0(),
   ));
 }
---
+++ lib/onboarding0.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter_application_1/onboarding1.dart';
+
+class Onboarding0 extends StatefulWidget {
+  const Onboarding0({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _OnboardingScreenState createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<Onboarding0>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(begin: 0.3, end: 1.0).animate(_controller);
+
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        // ignore: use_build_context_synchronously
+        context,
+        MaterialPageRoute(builder: (context) => OnboardingScreen1()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color.fromARGB(255, 0, 0, 0), Color(0xFF55244A)],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FadeTransition(
+                opacity: _animation,
+                child: Image.asset(
+                  'assets/logo.png', // Ensure your logo is in the assets folder
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                "Skill Swap",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
